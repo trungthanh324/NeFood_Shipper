63b5f2bbceabe75b2a23a3055826b81ac429461b
trungthanh324 <136559582+trungthanh324@users.noreply.github.com>
2024-10-27 14:36:05 +0700

    Initial Commit

---
+++ NeFoodforDeliverier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NeFoodforDeliverier
+//
+//  Created by Trung Thành  on 27/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NeFoodforDeliverier/NeFoodforDeliverierApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeFoodforDeliverierApp.swift
+//  NeFoodforDeliverier
+//
+//  Created by Trung Thành  on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NeFoodforDeliverierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
